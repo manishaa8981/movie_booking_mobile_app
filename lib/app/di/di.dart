5bba2d0b33dd461cde0ae57d2800eaf28e9f6ecd
manishaa8981 <manishaa8981@gmail.com>
2025-01-31 15:20:14 +0545

    api end point for connection backend and forntend

--- lib/app/di/di.dart
+++ lib/app/di/di.dart
@@ -1,24 +1,43 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:movie_ticket_booking/app/shared_prefs/token_shared_prefs.dart';
+import 'package:movie_ticket_booking/core/network/api_service.dart';
 import 'package:movie_ticket_booking/core/network/hive_service.dart';
-import 'package:movie_ticket_booking/features/auth/data/data_source/local_data_source/auth_local_datasource.dart';
+import 'package:movie_ticket_booking/features/auth/data/data_source/local_datasource/auth_local_datasource.dart';
+import 'package:movie_ticket_booking/features/auth/data/data_source/remote_datasource/auth_remote_datasource.dart';
 import 'package:movie_ticket_booking/features/auth/data/repository/auth_local_repository/auth_local_repository.dart';
+import 'package:movie_ticket_booking/features/auth/data/repository/auth_remote_repository.dart/auth_remote_repository.dart';
 import 'package:movie_ticket_booking/features/auth/domain/use_case/login_usecase.dart';
 import 'package:movie_ticket_booking/features/auth/domain/use_case/register_user_usecase.dart';
+import 'package:movie_ticket_booking/features/auth/domain/use_case/upload_image_usecase.dart';
 import 'package:movie_ticket_booking/features/auth/presentation/view_model/login/login_bloc.dart';
-import 'package:movie_ticket_booking/features/auth/presentation/view_model/on_boarding/on_boarding_cubit.dart';
 import 'package:movie_ticket_booking/features/auth/presentation/view_model/signup/register_bloc.dart';
 import 'package:movie_ticket_booking/features/home/presentation/view_model/home_cubit.dart';
+import 'package:movie_ticket_booking/features/splash/presentation/view_model/on_boarding/on_boarding_cubit.dart';
 import 'package:movie_ticket_booking/features/splash/presentation/view_model/splash_cubit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> initDependencies() async {
   await _initHiveService();
+  await _initApiService();
+  await _initSharedPreferences();
   _initHomeDependencies();
   _initRegisterDependencies();
   _initLoginDependencies();
   _initSplashScreenDependencies();
   _initOnboardingScreenDependencies();
+}
+
+Future<void> _initSharedPreferences() async {
+  final sharedPreferences = await SharedPreferences.getInstance();
+  getIt.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
+}
+
+_initApiService() {
+  //Remote Data Source
+  getIt.registerLazySingleton<Dio>(() => ApiService(Dio()).dio);
 }
 
 _initHiveService() {
@@ -31,27 +50,50 @@ void _initOnboardingScreenDependencies() {
   );
 }
 
+/// ====================  Register ===================
+
 _initRegisterDependencies() {
-  // init local data source
-  getIt.registerLazySingleton(
+  //DataSource
+
+  getIt.registerLazySingleton<AuthLocalDataSource>(
     () => AuthLocalDataSource(getIt<HiveService>()),
   );
 
-  // init local repository
+  getIt.registerLazySingleton<AuthRemoteDatasource>(
+    () => AuthRemoteDatasource(getIt<Dio>()),
+  );
+
+  //Repository
+
   getIt.registerLazySingleton(
     () => AuthLocalRepository(getIt<AuthLocalDataSource>()),
   );
+  getIt.registerLazySingleton<AuthRemoteRepository>(
+    () => AuthRemoteRepository(getIt<AuthRemoteDatasource>()),
+  );
 
-  // register use usecase
+  //UseCase
   getIt.registerLazySingleton<RegisterUseCase>(
     () => RegisterUseCase(
-      getIt<AuthLocalRepository>(),
+      getIt<AuthRemoteRepository>(),
     ),
   );
+
+  getIt.registerLazySingleton<UploadImageUsecase>(
+    () => UploadImageUsecase(
+      getIt<AuthRemoteRepository>(),
+    ),
+  );
+
   getIt.registerFactory<RegisterBloc>(
-    () => RegisterBloc(registerUseCase: getIt()),
+    () => RegisterBloc(
+      registerUseCase: getIt(),
+      uploadImageUsecase: getIt(),
+    ),
   );
 }
+
+// ================================ Home ==========================
 
 void _initHomeDependencies() {
   getIt.registerFactory<HomeCubit>(
@@ -59,18 +101,18 @@ void _initHomeDependencies() {
   );
 }
 
-// void _initRegisterDependencies() {
-//   getIt.registerFactory<RegisterBloc>(
-//     () => RegisterBloc(loginBloc: getIt<LoginBloc>()),
-//   );
-// }
+// =============================Login ============================
 
 _initLoginDependencies() async {
-  getIt.registerLazySingleton<LoginUseCase>(
-    () => LoginUseCase(
-      getIt<AuthLocalRepository>(),
-    ),
+  //Token Shared Preferences
+  getIt.registerLazySingleton<TokenSharedPrefs>(
+    () => TokenSharedPrefs(getIt<SharedPreferences>()),
   );
+
+  //UseCase
+
+  getIt.registerLazySingleton<LoginUseCase>(() =>
+      LoginUseCase(getIt<TokenSharedPrefs>(), getIt<AuthRemoteRepository>()));
 
   getIt.registerFactory<LoginBloc>(
     () => LoginBloc(
