b6c16cbb5355e707b3ec377c78b5ca8449734606
manishaa8981 <manishaa8981@gmail.com>
2025-01-31 15:27:32 +0545

    added upload image in repo and usecase

--- lib/features/auth/data/repository/auth_local_repository/auth_local_repository.dart
+++ lib/features/auth/data/repository/auth_local_repository/auth_local_repository.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:dartz/dartz.dart';
 import 'package:movie_ticket_booking/core/error/failure.dart';
-import 'package:movie_ticket_booking/features/auth/data/data_source/local_data_source/auth_local_datasource.dart';
+import 'package:movie_ticket_booking/features/auth/data/data_source/local_datasource/auth_local_datasource.dart';
 import 'package:movie_ticket_booking/features/auth/domain/entity/auth_entity';
 import 'package:movie_ticket_booking/features/auth/domain/repository/auth_repository.dart';
 
@@ -10,7 +10,6 @@ class AuthLocalRepository implements IAuthRepository {
   final AuthLocalDataSource _authLocalDataSource;
 
   AuthLocalRepository(this._authLocalDataSource);
-
 
   @override
   Future<Either<Failure, AuthEntity>> getCurrentUser() async {
@@ -38,14 +37,13 @@ class AuthLocalRepository implements IAuthRepository {
   }
 
   @override
-  Future<Either<Failure, String>> loginUser(String username, String password) async {
+  Future<Either<Failure, String>> loginUser(
+      String username, String password) async {
     try {
       final token = await _authLocalDataSource.loginUser(username, password);
-      return Right(token);
+      return (Right(token));
     } catch (e) {
       return Left(LocalDatabaseFailure(message: e.toString()));
     }
   }
-
-
 }
