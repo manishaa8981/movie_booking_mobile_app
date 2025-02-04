e9f700829e30e9395bca0d03ede921b97c19d697
manishaa8981 <manishaa8981@gmail.com>
2025-02-04 18:42:55 +0545

    created authrepo mock for mock test

---
+++ test/features/auth/domain/use_case/auth_repo.mock.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:movie_ticket_booking/features/auth/domain/repository/auth_repository.dart';
+
+class MockAuthRepository extends Mock implements IAuthRepository {}
